b7ce39d4b8a3ebac49d6aaabcf2ed6adcccf7abb
krishna <krishna.kondoju007@gmail.com>
2018-12-23 23:36:39 -0500

    add

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+    render () {
+        return (
+            <div>
+                tihi si sheader
+            </div>
+        )
+    }
+}
+
+export default Header
